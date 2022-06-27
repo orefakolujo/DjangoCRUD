45f5d233b80770f1fe5aaa5232f8305dc763a0dd
orefakolujo <orefakolujo@gmail.com>
2022-06-27 01:27:56 +0100

    The solution to the DjangoCrud assignment

---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+
+# Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
